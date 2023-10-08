7950829e34303c2f8ffbfc5f904a760b25cd3910
annatsygelnikova <143995230+annatsygelnikova@users.noreply.github.com>
2023-10-08 13:31:27 +0300

    Update script.js

--- web 5/script.js
+++ web 5/script.js
@@ -1,29 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var sprice = document.getElementById("sprice");
-    sprice.addEventListener("click", calculator);
-  });
+  var sprice = document.getElementById("sprice");
+  sprice.addEventListener("click", calculator);
+});
 
-  function validate() {
-    var count = document.getElementById("count").value;
-    var o = /^[\d]$/;
-    var valid = o.test(count);
+function validate() {
+  var count = document.getElementById("count").value;
+  var o = /^\d+$/;
+  var valid = o.test(count);
 
-    if(valid) output = "Данные введены верно";
-    else output = "Данные введены неверно. Повторите попытку ввода!";
-    document.getElementById("message").innerHTML = output;
-    return valid;
+  if (valid) {
+    output = "Данные введены верно";
+  } else {
+    output = "Данные введены неверно. Повторите попытку ввода!";
   }
+  document.getElementById("message").innerHTML = output;
+  return valid;
+}
 
-  function calculator() {
-    var name = document.getElementById("select");
-    var count = document.getElementById("count");
-    var result = document.getElementById("result");
+function calculator() {
+  var name = document.getElementById("select");
+  var count = document.getElementById("count");
+  var result = document.getElementById("result");
 
-    if(validate()){
-        var price = 0;
-        price += parseInt(name.options[name.selectedIndex].value);
-        price = parseInt(count.value) * price;
-        result.innerHTML = price;
-    }
-    else alert("Не удалось произвести вычисления!");
+  if (validate()) {
+    var price = 0;
+    price += parseInt(name.options[name.selectedIndex].value);
+    price = parseInt(count.value) * price;
+    result.innerHTML = price;
+  } else {
+    alert("Не удалось произвести вычисления!");
   }
+}
