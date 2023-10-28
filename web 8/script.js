de5d4df684f04d071a19af069b6423a9ecb38d69
annatsygelnikova <143995230+annatsygelnikova@users.noreply.github.com>
2023-10-28 17:03:47 +0300

    Update script.js

--- web 8/script.js
+++ web 8/script.js
@@ -1,28 +1,21 @@
 const button = document.querySelector("button");
 const form = document.querySelector("#myForm");
 const popup = document.querySelector(".popup");
+const formData = new FormData();
+    formData.append("Сообщение: ", "F");
+    const jsonString = JSON.stringify(Object.fromEntries(formData));
+
+    $.ajax({
+        url: 'https://api.slapform.com/hkhNWFrrb',
+        method: 'POST',
+        data: jsonString,
+        dataType: "json",
+        error: (e) => {
+            console.log("Ошибка");
+        }
+    });
 
 button.addEventListener("click", () => {
     form.classList.add("open");
     popup.classList.add("popup_open");
 });
-
-function validateForm() {
-    const inputs = document.querySelectorAll("input");
-    let isValid = true;
-
-    inputs.forEach((input) => {
-        if (input.value === "") {
-            isValid = false;
-            input.classList.add("error");
-        } else {
-            input.classList.remove("error");
-        }
-    });
-
-    if (!isValid) {
-        alert("Пожалуйста, заполните все поля формы!");
-    }
-
-    return isValid;
-}
